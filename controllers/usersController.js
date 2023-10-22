c51a95f1e7a325cbdb654b47408b4f5949bab501
Vignesh Kumar <reactivicky@gmail.com>
2023-10-22 07:04:39 +0530

    added update user route

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -23,6 +23,38 @@ const getUser = async (req, res) => {
   }
 };
 
-const updateUser = () => {};
+const updateUser = async (req, res) => {
+  const { id } = req.params;
+  const { firstName, lastName, planet, designation } = req.body;
+  try {
+    // We should do something like this if we are using mongoose
+    // const user = await User.findByIdAndUpdate(id, req.body, {
+    //   new: true,
+    //   runValidators: true,
+    // });
+
+    let user = users.find((u) => u.id === id);
+    user = { ...user, firstName, lastName, planet, designation };
+
+    if (!user) {
+      res.status(404).json({
+        status: "failed",
+        message: `No user with id ${id}`,
+      });
+    }
+
+    res.status(200).json({
+      status: "success",
+      data: {
+        user,
+      },
+    });
+  } catch (exports) {
+    res.status(404).json({
+      status: "failed",
+      message: `No user with id ${id}`,
+    });
+  }
+};
 
 module.exports = { getUser, updateUser };
